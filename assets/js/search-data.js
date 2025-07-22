6c355ceb23e9d72d745bb3d93887e38eaec2e427
miaomiaoChen2025 <100275023+miaomiaoChen2025@users.noreply.github.com>
2025-07-22 07:59:39 +0000

    Deploying to gh-pages from @ miaomiaoChen2025/miaomiaoChen2025.github.io@a941c1615be098fbbf55e769651ecb8c870d2b9a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,16 +70,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-school-of-information-management-wuhan-university-master-and-phd-in-information-science",
+          title: 'School of Information Management, Wuhan University..  Master and PhD in Information Science.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
